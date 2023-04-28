410730b38d1cebeca1766aef4beaa826c9acf70d
Tristan Green <trigre555@gmail.com>
2023-04-28 23:29:53 +0100

    Works
    General Works, cleaned up some redundant comments, and commented out all DevFee related bits until script is out of Beta.
    
    Still to come ---
    Custom TokenA to TokenB swaps. (Currently only works with SOL-USDC)

--- gridbot.js
+++ gridbot.js
@@ -10,12 +10,6 @@ import { Wallet } from '@project-serum/anchor';
 import axios from 'axios';
 import { promisify }  from 'util';
 
-//let selectedAddress; // initialize variable to store the selected address
-//let selectedToken = "";
-//let gridSpread;
-//let devFee;
-//let refreshTime;
-
 async function getTokens() {
     try {
         const response = await axios.get('https://token.jup.ag/strict');
@@ -30,12 +24,9 @@ async function getTokens() {
 }
 
 dotenv.config();
-
 //read keypair and decode to public and private keys.
 const wallet = new Wallet(Keypair.fromSecretKey(bs58.decode(process.env.PRIVATE_KEY)));
-
 // Replace with the Solana network endpoint URL
-
 const connection = new Connection(process.env.RPC_ENDPOINT, 'confirmed', {
     commitment: 'confirmed',
     timeout: 60000
@@ -68,7 +59,7 @@ class PriceResponse {
 let selectedAddress;
 let selectedToken = "";
 let gridSpread = 1;
-let devFee = 0.1;
+//let devFee = 0.1;
 let fixedSwapVal = 0;
 let slipTarget = 0.5;
 let refreshTime = 5;
@@ -117,6 +108,7 @@ async function main() {
                 },
                 filter: Number
             },
+            /*
             {
                 type: "input",
                 name: "devFee",
@@ -128,12 +120,14 @@ async function main() {
                 },
                 filter: Number
             }
+            */
         ];
 
         let answer = await inquirer.prompt(question);
 
         gridSpread = answer.gridSpread;
-        devFee = answer.devFee;
+        //devFee = answer.devFee;
+        
 
         const question2 =
             [
@@ -181,26 +175,13 @@ async function main() {
             
             console.log(`Selected Token: ${selectedToken}`);
             console.log(`Selected Grid Spread: ${gridSpread}%`);
-            console.log(`Selected Developer Donation: ${devFee}%`);
+            //console.log(`Selected Developer Donation: ${devFee}%`);
             console.log(`Swapping ${fixedSwapVal} ${selectedToken} per layer.`);
             console.log(`Slippage Target: ${slipTarget}%`)
-            console.log("");
-            /*
-            await (async () => {
-            const solBalance = await connection.getBalance(wallet.publicKey);
-            //const usdcBalance = await connection.getParsedTokenAccountsByOwner(wallet.publicKey, { mint: usdcMintAddress, })
-            const solBalanceStart = solBalance / 1000000000;
-            //const usdcBalanceStart = usdcBalance / 1000000;
-            console.log(`SOL Balance: ${solBalanceStart}`);
-            console.log(`USDC Balance: ${usdcBalanceStart}`);
-        })();
-        */
-            break;
-            
-        }
-        
-    refresh(selectedToken);   
-
+            console.log("");            
+            break;            
+        }        
+    refresh(selectedToken);
     setInterval(() => {
         refresh(selectedToken);
     }, refreshTime * 1000);
@@ -209,7 +190,7 @@ async function main() {
 //Init Spread Calculation once and declare spreads
 var gridCalc = true;
 let spreadUp, spreadDown, spreadIncrement;
-let solBalance, usdcBalance, solBalanceStart, usdcBalanceStart;
+let solBalance, usdcBalance, solBalanceStart, usdcBalanceStart, accountBalUSDStart, accountBalUSDCurrent;
 let buyOrders, sellOrders;
 var currentPrice;
 var lastPrice;
@@ -241,7 +222,7 @@ async function refresh(selectedToken) {
             console.log("");
             console.log("Settings:");
             console.log(`Grid Width: ${gridSpread}%`);
-            console.log(`Developer Donation: ${devFee}%`);
+            //console.log(`Developer Donation: ${devFee}%`);
             console.log(`Swapping ${fixedSwapVal}${selectedToken} per Grid`);
             console.log(`Maximum Slippage: ${slipTarget}%`);
             console.log("");
@@ -261,34 +242,40 @@ async function refresh(selectedToken) {
                 await (async () => {
                     const solBalance = await connection.getBalance(wallet.publicKey);
                     solBalanceStart = solBalance / 1000000000;
-                    console.log(`SOL Balance: ${solBalanceStart}`);
+                    console.log(`SOL Balance: ${solBalanceStart.toFixed(4)}`);
                 })();
                 if (!usdcBalanceStart) {
                     const usdcAccounts = await connection.getParsedTokenAccountsByOwner(wallet.publicKey, { mint: usdcMintAddress });
                     const usdcAccountInfo = usdcAccounts && usdcAccounts.value[0] && usdcAccounts.value[0].account;
                     const usdcTokenAccount = usdcAccountInfo.data.parsed.info;
                     usdcBalanceStart = usdcTokenAccount.tokenAmount.uiAmount;                    
-                }                
+                }     
+                accountBalUSDStart = (solBalanceStart * currentPrice) + usdcBalanceStart;
                 gridCalc = false;                
             }
-
-
-            console.log(`SOL Start Balance: ${solBalanceStart}`);
-            console.log(`USDC Start Balance: ${usdcBalanceStart}`);
+            console.log(`TokenA Start Balance: ${solBalanceStart.toFixed(4)}`);
+            console.log(`TokenB Start Balance: ${usdcBalanceStart.toFixed(4)}`);
+            console.log("");
 
             await (async () => {
                 const balance = await connection.getBalance(wallet.publicKey);
                 const currentBalance = balance / 1000000000
-                console.log(`Current SOL Balance: ${currentBalance}`);
+                console.log(`Current TokenA Balance: ${currentBalance}`);
                 const usdcAccounts = await connection.getParsedTokenAccountsByOwner(wallet.publicKey, { mint: usdcMintAddress });
                 const usdcAccountInfo = usdcAccounts && usdcAccounts.value[0] && usdcAccounts.value[0].account;
                 const usdcTokenAccount = usdcAccountInfo.data.parsed.info;
                 const currentUsdcBalance = usdcTokenAccount.tokenAmount.uiAmount;
-                console.log(`Current USDC Balance: ${currentUsdcBalance}`);
-                var solDiff = (currentBalance - solBalanceStart);
-                var usdcDiff = (currentUsdcBalance - usdcBalanceStart);
-                var profit = (solDiff * currentPrice) + usdcDiff;                
-                console.log(`Current Profit USD: ${profit}`)
+                accountBalUSDCurrent = (currentBalance * currentPrice) + currentUsdcBalance;
+                console.log(`Current TokenB Balance: ${currentUsdcBalance.toFixed(4)}`);
+                console.log("");
+                console.log(`Start Total USD Balance: ${accountBalUSDStart.toFixed(4)}`);
+                console.log(`Current Total USD Balance: ${accountBalUSDCurrent.toFixed(4)}`);
+                //var solDiff = (currentBalance - solBalanceStart);
+                //var usdcDiff = (currentUsdcBalance - usdcBalanceStart);
+                //var profit = (solDiff * currentPrice) + usdcDiff;
+                var profit = accountBalUSDCurrent - accountBalUSDStart;
+                console.log("");
+                console.log(`Current Profit USD: ${profit.toFixed(4)}`)                
                 console.log("");
                 console.log(`Buy Orders: ${buyOrders}`);
                 console.log(`Sell Orders: ${sellOrders}`);
@@ -339,11 +326,7 @@ async function refresh(selectedToken) {
         console.log(`Request failed with status code ${response.status}`);
     }
 }
-
-
-
 async function makeSellTransaction() {
-    var usdcLamports = fixedSwapVal * currentPrice;
     var fixedSwapValLamports = fixedSwapVal * 1000000000;
     var slipBPS = slipTarget * 100;
     // retrieve indexed routed map
@@ -356,12 +339,6 @@ async function makeSellTransaction() {
     Object.keys(indexedRouteMap['indexedRouteMap']).forEach((key, index) => {
         generatedRouteMap[getMint(key)] = indexedRouteMap["indexedRouteMap"][key].map((index) => getMint(index))
     });
-
-    console.log(selectedAddress);
-    console.log(slipBPS);
-    console.log(fixedSwapValLamports);
-    console.log(usdcLamports);
-
     const { data } = await (await fetch('https://quote-api.jup.ag/v4/quote?inputMint=' + selectedAddress + '&outputMint=EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v&amount=' + fixedSwapValLamports + '&slippageBps=' + slipBPS)).json();
     const routes = data;
 
@@ -375,32 +352,23 @@ async function makeSellTransaction() {
                 // route from /quote api
                 route: routes[0],
                 // user public key to be used for the swap
-                userPublicKey: wallet.publicKey.toString(),
-                // auto wrap and unwrap SOL. default is true
-                wrapUnwrapSOL: true,
-                // feeAccount is optional. Use if you want to charge a fee.  feeBps must have been passed in /quote API.
-                // This is the ATA account for the output token where the fee will be sent to. If you are swapping from SOL->USDC then this would be the USDC ATA you want to collect the fee.
-                // feeAccount: "fee_account_public_key"  
+                userPublicKey: wallet.publicKey.toString(),                
+                wrapUnwrapSOL: true,                
             })
         })
-    ).json();
-    console.log(transactions);
-
-    const { swapTransaction } = transactions;
-
-    console.log(swapTransaction);
+    ).json();    
+    const { swapTransaction } = transactions;    
     // deserialize the transaction
     const swapTransactionBuf = Buffer.from(swapTransaction, 'base64');
     var transaction = VersionedTransaction.deserialize(swapTransactionBuf);
-    console.log(transaction);
-
+    console.log("Making Sell Order!")
     // sign the transaction
     transaction.sign([wallet.payer]);
     // Execute the transaction
     const rawTransaction = transaction.serialize()
     const txid = await connection.sendRawTransaction(rawTransaction, {
-        skipPreflight: true,
-        maxRetries: 2
+        skipPreflight: false,
+        maxRetries: 5
     });
     await connection.confirmTransaction(txid);
     console.log(`https://solscan.io/tx/${txid}`);
@@ -408,13 +376,7 @@ async function makeSellTransaction() {
 }
 
 async function makeBuyTransaction() {
-    console.log(fixedSwapVal);
-    console.log(currentPrice);
-    var testLamp = (fixedSwapVal * currentPrice);
     var usdcLamports = Math.floor((fixedSwapVal * currentPrice) * 1000000);
-    console.log(testLamp);
-    console.log(usdcLamports);
-    //var fixedSwapValLamports = fixedSwapVal * 1000000000;
     var slipBPS = slipTarget * 100;
     // retrieve indexed routed map
     const indexedRouteMap = await (await fetch('https://quote-api.jup.ag/v4/indexed-route-map')).json();
@@ -426,14 +388,8 @@ async function makeBuyTransaction() {
     Object.keys(indexedRouteMap['indexedRouteMap']).forEach((key, index) => {
         generatedRouteMap[getMint(key)] = indexedRouteMap["indexedRouteMap"][key].map((index) => getMint(index))
     });
-
-    console.log(selectedAddress);
-    console.log(slipBPS);
-    console.log(usdcLamports);
-
     const { data } = await (await fetch('https://quote-api.jup.ag/v4/quote?inputMint=EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v&outputMint=' + selectedAddress + '&amount=' + usdcLamports + '&slippageBps=' + slipBPS)).json();
     const routes = data;
-    // console.log(routes)
     // get serialized transactions for the swap
     const transactions = await (
         await fetch('https://quote-api.jup.ag/v4/swap', {
@@ -446,11 +402,7 @@ async function makeBuyTransaction() {
                 route: routes[0],
                 // user public key to be used for the swap
                 userPublicKey: wallet.publicKey.toString(),
-                // auto wrap and unwrap SOL. default is true
-                wrapUnwrapSOL: true,
-                // feeAccount is optional. Use if you want to charge a fee.  feeBps must have been passed in /quote API.
-                // This is the ATA account for the output token where the fee will be sent to. If you are swapping from SOL->USDC then this would be the USDC ATA you want to collect the fee.
-                // feeAccount: "fee_account_public_key"  
+                wrapUnwrapSOL: true,                
             })
         })
     ).json();
@@ -459,15 +411,14 @@ async function makeBuyTransaction() {
     // deserialize the transaction
     const swapTransactionBuf = Buffer.from(swapTransaction, 'base64');
     var transaction = VersionedTransaction.deserialize(swapTransactionBuf);
-    console.log(transaction);
-
+    console.log("Making Buy Order!");
     // sign the transaction
     transaction.sign([wallet.payer]);
     // Execute the transaction
     const rawTransaction = transaction.serialize()
     const txid = await connection.sendRawTransaction(rawTransaction, {
-        skipPreflight: true,
-        maxRetries: 2
+        skipPreflight: false,
+        maxRetries: 5
     });
     await connection.confirmTransaction(txid);
     console.log(`https://solscan.io/tx/${txid}`);
